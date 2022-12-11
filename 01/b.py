9854f14418e30ae4e9f5a302fb81fdd62710d4d6
Mark Huggins <mark.huggins@anz.com>
2022-12-11 23:43:17 +1100

    fix typing in 01

--- 01/b.py
+++ 01/b.py
@@ -1,10 +1,11 @@
 from itertools import takewhile
+from typing import Iterable
 
 
 with open('01/input.txt') as f:
     lines = map(lambda x: x.rstrip(), f.readlines())
 
-def process(l) -> list:
+def process(l) -> Iterable[int]:
     while(True):
         if not (a := sum(map(int, takewhile(lambda x: len(x), lines)))):
             break
