99dcaaf81adb0b25c064ac4eb97f029b1d1f8e1a
Mark Huggins <mark.huggins@anz.com>
2022-12-11 23:31:47 +1100

    01a Passed

---
+++ 01/a.py
@@ -0,0 +1,13 @@
+from itertools import takewhile
+
+
+with open('01/input.txt') as f:
+    lines = map(lambda x: x.rstrip(), f.readlines())
+
+def process(l) -> list:
+    while(True):
+        if not (a := sum(map(int, takewhile(lambda x: len(x), lines)))):
+            break
+        yield a
+
+print(max(process(lines)))
